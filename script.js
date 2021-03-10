044f38bc2e118895c31affef982e7896f1c5dc3d
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-10 09:46:37 -0500

    added more special characters for passwords

--- script.js
+++ script.js
@@ -18,20 +18,22 @@ function generatePassword() {
   var generatedPwd = "";
   var pwdLength = prompt("What should be the length of your password? Between 8 and 128 characters.");
 
+  // verify user's password length input
   if (pwdLength < 8 || pwdLength > 128 || isNaN(pwdLength)) {
     alert("Please enter a number between 8 and 128.");
     return;
   }
   
-
+  // prompts for password criteria
   var specialChar = confirm("Would you like to include special characters in your password?");
   var upperChar = confirm("Would you like to include upper case characters in your password?");
   var lowerChar = confirm("Would you like to include lower case characters in your password?");
   var numChar = confirm("Would you like to include numbers in your password?");
 
+  // define characters based on criteria
   var lwrCase = "abcdefghijklmnopqrstuvxwyz";
   var uprCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var spcChar = "!@#$%&*";
+  var spcChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   var numbers = "0123456789";
 
   if(specialChar) {
@@ -55,11 +57,12 @@ function generatePassword() {
     return;
   }
 
+  // generate a random password from combined strings
   for (i=0; i<pwdLength ; i++) {
     generatedPwd += pwd.charAt(Math.floor(Math.random() * pwd.length));
   }
 
   
-
+  // return generated password
   return generatedPwd;
 }
